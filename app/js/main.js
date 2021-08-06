fb68fc447dd93c60fdd484c0ff28606791345405
Ahtoxatlt <ahtoxa84@list.ru>
2021-08-06 15:55:38 +0400

    added slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,9 @@
 $(function(){
-
-
-
+    const swiper = new Swiper('.swiper-container', {
+        spaceBetween: 50,
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+          },
+      });
 });
