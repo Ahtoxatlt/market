5e5f24fbeadccf68320e9913175dbf969c18b306
Ahtoxatlt <ahtoxa84@list.ru>
2021-09-23 11:17:20 +0400

    123

--- app/js/main.js
+++ app/js/main.js
@@ -1,9 +1,43 @@
 $(function(){
-    const swiper = new Swiper('.swiper-container', {
+    const swiper = new Swiper('.slider-one-cart', {
         spaceBetween: 50,
         navigation: {
-            nextEl: '.swiper-button-next',
-            prevEl: '.swiper-button-prev',
+            nextEl: '.slider-btn-next',
+            prevEl: '.slider-btn-prev',
+          },
+      });
+
+      var mixer = mixitup('.release__cart');
+
+      const sliderCart = new Swiper('.slider-swiper-cart', {
+        slidesPerView: 3,
+        slidesPerGroup: 3,
+        spaceBetween: 28,
+        navigation: {
+            nextEl: '.slider-cart-btn-next',
+            prevEl: '.slider-cart-btn-prev',
+          },
+      });
+
+      const sliderFeedback = new Swiper('.feedback__inner-cart', {
+        slidesPerView: 2,
+        slidesPerGroup: 2,
+        spaceBetween: 28,
+        navigation: {
+            nextEl: '.feedback__btn-next',
+            prevEl: '.feedback__btn-prev',
           },
       });
 });
+
+$(function () {
+ 
+    $(".rate-star").rateYo({
+      
+      rating: 4.5,
+    starWidth: "15px",
+    spacing: "1px",
+    readOnly: true,
+    });
+   
+  });
